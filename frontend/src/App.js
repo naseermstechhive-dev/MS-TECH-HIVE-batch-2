91746e80128580a1cc8c7448c6e59f5eb848cb86
swatisaulanki <swatisaulanki@gmail.com>
2025-11-12 12:13:51 +0530

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <div className="App">
+  <div className="text-3xl font-bold text-blue-500 bg-yellow-200 p-4 rounded-lg">
+    MS-TECH HIVE
+  </div>
+</div>
+
+  );
+}
+
+export default App;
